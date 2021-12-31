2378b9512548a2e4e84fa43ebbe659d627b6121e
Leite <joao.augusto@picpay.com>
2021-12-31 19:05:11 -0300

    getting media and starting geo

--- utils.py
+++ utils.py
@@ -44,7 +44,32 @@ def collect_tweets_from_query(gdrive, client, query, max_results, news_id, save_
             'public_metrics',
             'verified',
         ],
-        expansions=["author_id", "referenced_tweets.id", "referenced_tweets.id.author_id"],
+        media_fields = [
+            "media_key",
+            "type",
+            "duration_ms",
+            "public_metrics",
+            "width",
+            "height",
+            "preview_image_url",
+            "alt_text"
+        ],
+        place_fields = [
+            "full_name",
+            "id",
+            "contained_within",
+            "country",
+            "country_code",
+            "geo",
+            "name",
+            "place_type"
+        ],
+        expansions = [
+            "author_id",
+            "referenced_tweets.id",
+            "referenced_tweets.id.author_id",
+            "attachments.media_keys"
+        ],
         max_results=100,
     )):
         print(f"Batch {i}")
@@ -52,49 +77,80 @@ def collect_tweets_from_query(gdrive, client, query, max_results, news_id, save_
             print(f"collected {len(collected_tweets)} tweets.")
             break
 
+        # expansions
         users = {u["id"]: u for u in tweets.includes['users']}
         referenced_tweets = {
             referenced_tweet.id: referenced_tweet for referenced_tweet in tweets.includes.get("tweets")
         }
+        media = {media["media_key"]: media for media in tweets.includes["media"]}
+        # places = {place["id"]: place for place in tweets.includes["places"]}
+
         for i, tweet in enumerate(tweets.data):
             new_row = {}
-            if users[tweet.author_id]:
-                user = users[tweet.author_id]
-                # get referenced tweet if its a retweet (otherwise text is truncated)
-                tweet_text = referenced_tweets[tweet.referenced_tweets[0].id].text if tweet.get("referenced_tweets") else tweet.text
-                new_row = {
-                    "tweet_text": tweet_text,
-                    "tweet_id": tweet.id,
-                    "tweet_created_at": tweet.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                    "tweet_geo": tweet.geo["place_id"] if tweet.geo else None,
-                    "tweet_public_metrics": tweet.public_metrics,
-                    "tweet_in_reply_to_user_id": tweet.in_reply_to_user_id,
-                    "tweet_conversation_id": tweet.conversation_id,
-                    "tweet_lang": tweet.lang,
-                    "author_id": tweet.author_id,
-                    "author_name": user.name,
-                    "author_username": user.username,
-                    "author_created_at": user.created_at.strftime("%Y-%m-%d %H:%M:%S"),
-                    "author_description": user.description,
-                    "author_entities": user.entities,
-                    "author_location": user.location,
-                    "author_is_protected": user.protected,
-                    "author_is_verified": user.verified,
-                    "author_profile_image_url": user.profile_image_url,
-                    "author_public_metrics": user.public_metrics
-                }
-                collected_tweets.append(new_row)
-    
-                if len(collected_tweets) >= save_batch_size:
-                    data = {
-                        "query": query,
-                        "query_id": FILE_UUID,
-                        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                        "data": collected_tweets,
-                    }
+        
+            user = users[tweet.author_id]
+            # place = places[tweet.geo["place_id"]] if tweet.geo.get("place_id") else None
+            attachments = tweet['attachments']
+            if attachments:
+                media_keys = attachments.get('media_keys')
+            else:
+                media_keys = None
 
-                    upload_file(gdrive, data, f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.json", news_id)
-                    collected_tweets = []
+            tweet_media = media.get(media_keys[0] if media_keys else media_keys)
+            # get referenced tweet if its a retweet (otherwise text is truncated)
+            tweet_text = referenced_tweets[tweet.referenced_tweets[0].id].text if tweet.get("referenced_tweets") else tweet.text
+            new_row = {
+                "tweet_text": tweet_text,
+                "tweet_id": tweet.id,
+                "tweet_created_at": tweet.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                "tweet_public_metrics": tweet.public_metrics,
+                "tweet_in_reply_to_user_id": tweet.in_reply_to_user_id,
+                "tweet_conversation_id": tweet.conversation_id,
+                "tweet_lang": tweet.lang,
+
+                "author_id": user.id,
+                "author_name": user.name,
+                "author_username": user.username,
+                "author_created_at": user.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+                "author_description": user.description,
+                "author_entities": user.entities,
+                "author_location": user.location,
+                "author_is_protected": user.protected,
+                "author_is_verified": user.verified,
+                "author_profile_image_url": user.profile_image_url,
+                "author_public_metrics": user.public_metrics,
+
+                "media_key": tweet_media.get("media_key") if tweet_media else None,
+                "media_type": tweet_media.get("media_type") if tweet_media else None,
+                "media_duration_ms": tweet_media.get("duration_ms") if tweet_media else None,
+                "media_height": tweet_media.get("height") if tweet_media else None,
+                "media_width": tweet_media.get("width") if tweet_media else None,
+                "media_preview_image_url": tweet_media.get("preview_image_url") if tweet_media else None,
+                "media_alt_text": tweet_media.get("alt_text") if tweet_media else None,
+                "media_view_count": (
+                    tweet_media.get("public_metrics").get("view_count")
+                        if tweet_media and 
+                            tweet_media.get("public_metrics") and
+                            tweet_media["public_metrics"].get("view_count")
+                        else None
+                ),
+
+                # "place_id": place.id,
+                # "place_full_name": place.full_name
+
+            }
+            collected_tweets.append(new_row)
+
+            if len(collected_tweets) >= save_batch_size:
+                data = {
+                    "query": query,
+                    "query_id": FILE_UUID,
+                    "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    "data": collected_tweets,
+                }
+
+                upload_file(gdrive, data, f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.json", news_id)
+                collected_tweets = []
 
     data = {
         "query": query,
